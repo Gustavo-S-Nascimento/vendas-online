7d9c5d0c5769c0af708bab7b024944e86231f47c
Gustavo-S-Nascimento <Guga_Vine@outlook.com>
2023-10-23 18:18:00 -0300

    Loja de vendas

--- main.js
+++ main.js
@@ -10,19 +10,106 @@ tituloDashboardElement.textContent = nome;
 spanBottomInfoElement.textContent = nome;
 
 if (nome === "Pascax") {
-    cardElement.style.backgroundColor = "#000"; 
-    signalElement.src = "./assets/wireless-signal-1119306_1280.webp"; 
-    spanBottomInfoElement.style.color = "#fff"; 
+    cardElement.style.backgroundColor = "#000";
+    signalElement.src = "./assets/wireless-signal-1119306_1280.webp";
+    spanBottomInfoElement.style.color = "#fff";
     premiumElement.style.display = "";
 }
 
-const Saldo = prompt("Digite o novo valor do saldo:");
-const saldoElement = document.getElementById("saldo");
+let saldo = prompt("Digite o Valor");
 
-const novoSaldoNumero = parseFloat(Saldo.replace("R$", ""));
+let recebe = parseFloat(saldo.replace("R$", ""))
 
-if (novoSaldoNumero) {
-    saldoElement.textContent = `R$${novoSaldoNumero.toFixed(2)}`;
-} else {
-    saldoElement.textContent = "Valor inválido";
+
+if (recebe !== null && recebe !== "") {
+    let saldoCliente = document.querySelector("#saldo");
+    saldoCliente.innerHTML = `R$ ${saldo}`
 }
+
+function atualizarSaldo() {
+    document.querySelector("#saldo").textContent = `R$ ${saldo.toFixed("2")}`;
+    saldo.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+}
+
+const historico = [];
+
+function registrarTransacao(item, valor) {
+    const data = new Date();
+    const dataFormatada = `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    const transacao = {
+        data: dataFormatada,
+        item: item,
+        valor: valor
+    };
+    historico.push(transacao);
+
+    // Atualize a lista de histórico no HTML
+    const listaHistorico = document.getElementById("lista-historico");
+    const li = document.createElement("li");
+    li.textContent = ` ${transacao.item} - R$ ${transacao.valor.toFixed(2)}`; // ${transacao.data}:
+    listaHistorico.appendChild(li);
+}
+
+function gastoBoneco() {
+    const valorBoneco = 150;
+    if (saldo >= valorBoneco) {
+        saldo -= valorBoneco;
+        atualizarSaldo();
+        registrarTransacao("Boneco", valorBoneco);
+    } else if (nome === "Pascax") {
+        document.querySelector("#ValorBoneco").style.color = "#DAA520";
+        saldo -= valorBoneco;
+        atualizarSaldo();
+        registrarTransacao("Boneco", valorBoneco);
+    }
+    else {
+        alert("Acabou o money!");
+        document.querySelector("#ValorBoneco").style.color = "red";
+        document.querySelector("#overlayBoneco").textContent = "Valor Insuficiente!";
+        document.querySelector("#overlayBoneco").classList.add("overlay-red");
+    }
+}
+function gastoNotebook() {
+    const valorNotebook = 6000;
+    if (saldo >= valorNotebook) {
+        saldo -= valorNotebook;
+        atualizarSaldo();
+        registrarTransacao("Notebook", valorNotebook);
+    } else if (nome === "Pascax") {
+        document.querySelector("#ValorNotebook").style.color = "#DAA520";
+        saldo -= valorNotebook;
+        atualizarSaldo();
+        registrarTransacao("Notebook", valorNotebook);
+    }
+    else {
+        document.querySelector("#ValorNotebook").style.color = "red";
+        document.querySelector("#overlayNotebook").textContent = "Valor Insuficiente!";
+        document.querySelector("#overlayNotebook").classList.add("overlay-red");
+        alert("Acabou o money!");
+    }
+}
+function gastoMonitor() {
+    const valorMonitor = 750;
+    if (saldo >= valorMonitor) {
+        saldo -= valorMonitor;
+        atualizarSaldo();
+        registrarTransacao("Monitor", valorMonitor);
+    } else if (nome === "Pascax") {
+        document.querySelector("#ValorMonitor").style.color = "#DAA520";
+        saldo -= valorMonitor;
+        atualizarSaldo();
+        registrarTransacao("Monitor", valorMonitor);
+    }
+    else {
+        document.querySelector("#ValorMonitor").style.color = "red";
+        document.querySelector("#overlayMonitor").textContent = "Valor Insuficiente!";
+        document.querySelector("#overlayMonitor").classList.add("overlay-red");
+        alert("Acabou o money!");
+    }
+}
+
+document.getElementById("boneco").addEventListener("click", gastoBoneco);
+document.getElementById("notebook").addEventListener("click", gastoNotebook);
+document.getElementById("monitor").addEventListener("click", gastoMonitor);
+
+atualizarSaldo();
